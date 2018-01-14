84020a783a23980c6487347dbf29632853e50cd8
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-01-14 23:07:28 +0700

    Fix migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_type');
+            $table->integer('user_type')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
