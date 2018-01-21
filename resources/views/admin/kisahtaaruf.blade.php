940348c76114c77f65437931001e146166abed35
faldyif <faldy.if@gmail.com>
2018-01-22 03:00:21 +0700

    views update (full)

---
+++ resources/views/admin/kisahtaaruf.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+@section('content')
+            <!-- BEGIN CONTENT -->
+            <div class="page-content-wrapper">
+                <!-- BEGIN CONTENT BODY -->
+                <div class="page-content">
+                    <!-- BEGIN PAGE HEAD-->
+                    <div class="page-head">
+                        <!-- BEGIN PAGE TITLE -->
+                        <div class="page-title">
+                            <h1>Kisah Ta'aruf</h1>
+                        </div>
+                        <!-- END PAGE TITLE -->
+                    </div>
+                    <!-- END PAGE HEAD-->
+                    <!-- BEGIN PAGE BREADCRUMB -->
+                    <ul class="page-breadcrumb breadcrumb">
+                        <li>
+                            <a href="index.html">Home</a>
+                            <i class="fa fa-circle"></i>
+                        </li>
+                        <li>
+                            <a href="#">Kisah Ta'aruf</a>
+                        </li>
+                    </ul>
+                    <!-- END PAGE BREADCRUMB -->
+                    <!-- BEGIN PAGE BASE CONTENT -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- BEGIN EXTRAS PORTLET-->
+                            <div class="portlet light form-fit">
+                                <div class="portlet-title">
+                                    <div class="caption font-dark">
+                                        <i class="icon-layers"></i>
+                                    <span class="caption-subject bold uppercase"> 
+                                        Kisah Ta'aruf
+                                    </span>
+                                    </div>
+                                </div>
+                                <div class="portlet-body m-heading-1 border-green m-bordered">
+                                    <div class="form-group">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                                        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+                                        in culpa qui officia deserunt mollit anim id est laborum
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                    <!-- END PAGE BASE CONTENT -->
+                </div>
+                <!-- END CONTENT BODY -->
+            </div>
+            <!-- END CONTENT -->
+        </div>
+        <!-- END CONTAINER -->
+        @endsection
