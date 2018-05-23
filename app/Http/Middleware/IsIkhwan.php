a894be4757ffa4629461e3f49f9cae9edd93b571
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-05-23 07:36:49 +0700

    Make additional ikhwan and akhwat middleware

---
+++ app/Http/Middleware/IsIkhwan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsIkhwan
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(! is_null($request->user()) && ! $request->user()->userType() == 'ikhwan') {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
